8f982c5303ba14619c36dbf0f45867c5e605a019
Edward <edward@smeathers.co.uk>
2026-06-07 01:09:28 +0100

    Update __init__.py

--- custom_components/trackr/__init__.py
+++ custom_components/trackr/__init__.py
@@ -2,7 +2,7 @@ DOMAIN = "trackr"
 
 
 async def async_setup(hass, config):
-    hass.states.async_set("hello_state.world", "Paulus")
+    hass.states.async_set("trackr.world", "Paulus")
 
     # Return boolean to indicate that initialization was successful.
     return True
