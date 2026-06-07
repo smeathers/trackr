cfd16fd6d76b6f6f9f1cf8583f8d859506b50340
Edward <edward@smeathers.co.uk>
2026-06-07 13:37:38 +0100

    Update config_flow.py

--- custom_components/trackr/config_flow.py
+++ custom_components/trackr/config_flow.py
@@ -1,7 +1,14 @@
+import copy
+import logging
+
+import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
 
 class TrackrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
+    def _show_main_form(self, errors=None):
+        return self._show_user_form("user", DOMAIN_SCHEMA, errors or {})
+        
     async def async_step_user(self, info):
