e5239bd0a8d1618ea271d8bdf33fb3c42369b267
Edward <edward@smeathers.co.uk>
2026-06-07 13:50:55 +0100

    Update __init__.py

--- custom_components/trackr/__init__.py
+++ custom_components/trackr/__init__.py
@@ -5,3 +5,14 @@ async def async_setup(hass, config):
 
     # Return boolean to indicate that initialization was successful.
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+        if not hass.config_entries.async_entries(DOMAIN):
+            hass.data.pop(DOMAIN)
+
+    return unload_ok
