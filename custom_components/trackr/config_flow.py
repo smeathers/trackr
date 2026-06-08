90d41b6b95280011127ca673a68fae4a3bc22403
Edward <edward@smeathers.co.uk>
2026-06-08 11:31:54 +0100

    Update config_flow.py

--- custom_components/trackr/config_flow.py
+++ custom_components/trackr/config_flow.py
@@ -22,4 +22,4 @@ class TrackrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     #def _show_main_form(self, errors=None):
     #    return self._show_user_form("user", DOMAIN_SCHEMA, errors or {})
         
-    async def async_step_user(self, info) -> config_entries.ConfigFlowResult:
+    async def async_step_user(self, user_input=None) -> config_entries.ConfigFlowResult:
