a2584bc246150838dbc05d2d27ff37d548d2f731
Edward <edward@smeathers.co.uk>
2026-06-08 11:18:57 +0100

    Update config_flow.py

--- custom_components/trackr/config_flow.py
+++ custom_components/trackr/config_flow.py
@@ -2,12 +2,9 @@ import copy
 import logging
 
 import voluptuous as vol
-from homeassistant.components.bluetooth import (
-    BluetoothServiceInfoBleak,
-    async_discovered_service_info,
-)
+from homeassistant.components.bluetooth import BluetoothServiceInfoBleak
 from homeassistant import config_entries
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+#from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from .const import DOMAIN
 
 
@@ -17,8 +14,10 @@ class TrackrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     def __init__(self) -> None:
-        """Set up a new config flow for Aranet."""
+        """Initialize the config flow."""
         self._discovery_info: BluetoothServiceInfoBleak | None = None
+        self._discovered_device: DeviceData | None = None
+        self._discovered_devices: dict[str, Discovery] = {}
         
     def _show_main_form(self, errors=None):
         return self._show_user_form("user", DOMAIN_SCHEMA, errors or {})
