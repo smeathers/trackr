647550917e44479645c873b1ccbcde4cc3774a53
Edward <edward@smeathers.co.uk>
2026-06-13 01:16:04 +0100

    Update config_flow.py

--- custom_components/trackr/config_flow.py
+++ custom_components/trackr/config_flow.py
@@ -12,17 +12,21 @@ class TrackrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
 
     VERSION = 1
-
-    def __init__(self) -> None:
-        """Initialize the config flow."""
-        self._discovery_info: BluetoothServiceInfoBleak | None = None
-        self._discovered_device: DeviceData | None = None
-        self._discovered_devices: dict[str, Discovery] = {}
-        
-    #def _show_main_form(self, errors=None):
-    #    return self._show_user_form("user", DOMAIN_SCHEMA, errors or {})
-        
-    async def async_step_user(self, user_input=None) -> ConfigFlowResult:
+    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
+        """Invoked when a user initiates a flow via the user interface."""
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            try:
+                await validate_auth(user_input[CONF_ACCESS_TOKEN], self.hass)
+            except ValueError:
+                errors["base"] = "auth"
+            if not errors:
+                # Input is valid, set data.
+                self.data = user_input
+                self.data[CONF_REPOS] = []
+                # Return the form of the next step.
+                return await self.async_step_repo()
+    
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
         )
