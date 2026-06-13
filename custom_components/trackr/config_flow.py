b287cc9a4cb4929ce86354259ee52deedce72481
Edward <edward@smeathers.co.uk>
2026-06-13 11:50:52 +0100

    Update config_flow.py

--- custom_components/trackr/config_flow.py
+++ custom_components/trackr/config_flow.py
@@ -15,17 +15,6 @@ class TrackrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Invoked when a user initiates a flow via the user interface."""
         errors: Dict[str, str] = {}
-        if user_input is not None:
-            try:
-                await validate_auth(user_input[CONF_ACCESS_TOKEN], self.hass)
-            except ValueError:
-                errors["base"] = "auth"
-            if not errors:
-                # Input is valid, set data.
-                self.data = user_input
-                self.data[CONF_REPOS] = []
-                # Return the form of the next step.
-                return await self.async_step_repo()
     
         return self.async_show_form(
             step_id="user", data_schema=AUTH_SCHEMA, errors=errors
