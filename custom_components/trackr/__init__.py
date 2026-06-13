ae1eb5a49f3490894170ec4eb6ee0ac8da6a5a09
Edward <edward@smeathers.co.uk>
2026-06-14 00:27:33 +0100

    Update __init__.py

--- custom_components/trackr/__init__.py
+++ custom_components/trackr/__init__.py
@@ -30,28 +30,4 @@ async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> 
 # TODO Update entry annotation
 async def async_unload_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)from .const import DOMAIN, PLATFORMS
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    #hass.states.async_set("trackr.world", "Paulus")
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-    
-    # Return boolean to indicate that initialization was successful.
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-        if not hass.config_entries.async_entries(DOMAIN):
-            hass.data.pop(DOMAIN)
-
-    return unload_ok
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await hass.config_entries.async_reload(entry.entry_id)
+    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
